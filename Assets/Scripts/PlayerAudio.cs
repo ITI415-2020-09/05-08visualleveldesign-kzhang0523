c48abdd66f29b3c51aaed7a6d3441aaa25b88c92
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-11-06 20:26:24 -0500

    Adjust Layers and Add music

---
+++ Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip splashSound;
+
+    public AudioSource audioS;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+    }
+}
