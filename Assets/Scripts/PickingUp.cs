b4bbe9377fe641f8a42d9b9f1877c608c0f216b5
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-11-06 21:28:38 -0500

    Make Fruits Avaiable for Pickup

---
+++ Assets/Scripts/PickingUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PickingUp : MonoBehaviour
+{
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+    void SetCountText()
+    {
+        countText.text = "Fruit Count: " + count.ToString();
+        if (count >= 12)
+        {
+
+            winTextObject.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Fruit"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+
+        }
+    }
+}
