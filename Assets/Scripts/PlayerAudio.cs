1b33c2d6c39b601ebdd8828f3835e51c368096cb
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-11-06 20:52:30 -0500

    Add Music

--- Assets/Scripts/PlayerAudio.cs
+++ Assets/Scripts/PlayerAudio.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerAudio : MonoBehaviour
 {
@@ -8,11 +9,30 @@ public class PlayerAudio : MonoBehaviour
 
     public AudioSource audioS;
 
+    public AudioMixerSnapshot mainSnapshot;
+    public AudioMixerSnapshot altSnapshot;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Water"))
         {
             audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("Interior"))
+        {
+            altSnapshot.TransitionTo(0.5f);
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("Interior"))
+        {
+            mainSnapshot.TransitionTo(0.5f);
         }
     }
 }
